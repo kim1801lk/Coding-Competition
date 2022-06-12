506cd77df3acbaec8a4153943d902928688c18e3
Lauren Kim <joohakim@gmail.com>
2022-06-12 18:22:53 -0400

    Attempt for Soldier and Bananas
    only worked for first test case...

---
+++ General/src/CFSoldier.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class CFSoldier {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		int k = input.nextInt();	//cost of first banana
+		int n = input.nextInt();	//dollars in possession
+		int w = input.nextInt();	//number of wanted bananas
+		int sum = 0;
+		
+//		int sum = (w/2) * (1 + w);
+//		
+//		int totalDollar = sum * k;
+		
+		for (int i = 1; i <= w; i++) {
+			sum = sum + i*k;
+		}
+		
+		System.out.println(sum - n);
+		
+
+	}
+
+}
