52b734600908fa132601e488134b8bbf49be876e
Lauren Kim <joohakim@gmail.com>
2022-06-29 22:05:38 -0400

    Update USACO Lost Cow

--- Bronze/src/USACOLostCow.java
+++ Bronze/src/USACOLostCow.java
@@ -8,28 +8,32 @@ public class USACOLostCow {
 		Scanner in = new Scanner (new FileReader("lostcow.in"));
 		PrintWriter out = new PrintWriter("lostcow.out");
 		
+		//only works for 4 test cases....
 		
 		int x = in.nextInt();
 		int y = in.nextInt();
 		
 		
+		int traveling = 1;
+		int totalDistance = 0;
+		int position = x;
+		int previousP = x;
 		
-		int iteration = 0;
-		int distance = 1;
-		
-		while (x < y) {
+		while (position < y) {
 			
-			x = x + distance;
 			
-			distance = distance * -2;
-			iteration++;
+			previousP = position;
+			position = x + traveling;
+			//out.println("current position " + position);
+			totalDistance = totalDistance + Math.abs(position - previousP);
+			//out.println("distance traveled: " + totalDistance);
+			traveling = traveling * -2;
+			
 		}
-	
-		int sum = (int) ((1- Math.pow(2, iteration)) / (1 - 2));
 		
 		
-		
-		out.println(sum + x - y + 2);
+		//out.println("final position " + position);
+		out.println(totalDistance - Math.abs(position - y));
 		
 		in.close();
 		out.close();
