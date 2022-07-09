91c46725a51a68df056e9dcdb00604481529199b
Lauren Kim <joohakim@gmail.com>
2022-07-08 20:25:40 -0400

    Update USACO ABC

--- General/src/USACOabc.java
+++ General/src/USACOabc.java
@@ -6,49 +6,17 @@ public class USACOabc {
 		Scanner input = new Scanner (System.in);
 		
 		
-		ArrayList <Integer> numbers = new ArrayList <Integer>();
+		int [] nums = new int [7];
 		
 		for (int i =0; i < 7;i++) {
-			numbers.add(input.nextInt());
+			nums [i] = input.nextInt();
 		}
 			
-		int a = numbers.get(0);
-		int indexA = 0;
-		for (int i =1; i < 7;i++) {
-			int num = numbers.get(i);
-			if (num < a) {
-				a = num;
-				indexA = i;
-				}
-		}
-		numbers.remove(indexA);
+		Arrays.sort(nums);
 		
 		
 		
-		int b = numbers.get(0);
-		int indexB = 0;
-		for (int i =1; i < 6;i++) {
-			int num = numbers.get(i);
-			if (num < b) {
-				b = num;
-				indexB = i;
-			}
-		}
-		numbers.remove(indexB);
-		
-		
-		int c = numbers.get(0);
-	
-		for (int i =1; i < 5;i++) {
-			int num = numbers.get(i);
-			if (num < c) {
-				if (num != a + b) {
-				c = num;
-				
-			}}
-		}
-		
-		System.out.printf("%d %d %d", a, b, c);
+		System.out.printf("%d %d %d", nums [0], nums[1], nums[6] - nums[0] - nums[1]);
 		
 		
 
